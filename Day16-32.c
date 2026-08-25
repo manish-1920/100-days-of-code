9f7a300225286885ef852e5ae01f7220de971b81
Manish <manishkolluru3024@icloud.com>
2026-08-25 19:16:18 +0530

    Day16-32 c program

---
+++ Day16-32.c
@@ -0,0 +1,31 @@
+/*
+Q32 (Loops without Arrays/Strings)
+
+Write a program to check if a number is a palindrome.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, original, remainder;
+    int reversed = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n > 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reversed) {
+        printf("%d is a palindrome.\n", original);
+    } else {
+        printf("%d is not a palindrome.\n", original);
+    }
+
+    return 0;
+}
