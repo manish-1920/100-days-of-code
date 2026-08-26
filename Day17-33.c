b0c38f31ff724d95da75f5ab777b93601fbfbb57
Manish <manishkolluru3024@icloud.com>
2026-08-26 19:00:21 +0530

    Day17-33 c program

---
+++ Day17-33.c
@@ -0,0 +1,31 @@
+/*
+Q33 (Loops without Arrays/Strings)
+
+Write a program to check if a number is an Armstrong number.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, original, remainder;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n > 0) {
+        remainder = n % 10;
+        sum = sum + remainder * remainder * remainder;
+        n = n / 10;
+    }
+
+    if (sum == original) {
+        printf("%d is an Armstrong number.\n", original);
+    } else {
+        printf("%d is not an Armstrong number.\n", original);
+    }
+
+    return 0;
+}
