f8fac193a21d97936dc8dc3a1ec88a8199de11fd
Manish <manishkolluru3024@icloud.com>
2026-09-12 14:34:04 +0530

    Day26-52 c program

---
+++ Day26-52.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 15; i++)
+    {
+        if(i == 1 || i == 5 || i == 10 || i == 15)
+        {
+            printf("*\n");
+        }
+        else
+        {
+            printf("*\n");
+        }
+    }
+
+    return 0;
+}
